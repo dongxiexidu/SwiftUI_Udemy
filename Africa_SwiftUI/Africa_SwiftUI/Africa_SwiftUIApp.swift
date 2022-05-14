45423331e36a0a4f80f624aacaa8ddad481509e7
PJunyeong <j_aion@naver.com>
2022-05-14 16:46:25 +0900

    chore: New Lec

---
+++ Africa_SwiftUI/Africa_SwiftUI/Africa_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Africa_SwiftUIApp.swift
+//  Africa_SwiftUI
+//
+//  Created by Junyeong Park on 2022/05/14.
+//
+
+import SwiftUI
+
+@main
+struct Africa_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Africa_SwiftUI/README.txt
@@ -0,0 +1,3 @@
+# Africa SwiftUI: Udemy SwiftUI Lecture
+
+* 2022-05-14 Start: Video App Making Lectures
