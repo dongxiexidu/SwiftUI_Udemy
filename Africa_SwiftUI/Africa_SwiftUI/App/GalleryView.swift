a3e900eafc0dd9740ddab74c23e23589f6fd1dae
PJunyeong <j_aion@naver.com>
2022-05-17 15:12:21 +0900

    style: Asset done

---
+++ Africa_SwiftUI/Africa_SwiftUI/App/GalleryView.swift
@@ -0,0 +1,20 @@
+//
+//  GalleryView.swift
+//  Africa_SwiftUI
+//
+//  Created by Junyeong Park on 2022/05/17.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text("Gallery View")
+    }
+}
+
+struct GalleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        GalleryView()
+    }
+}
---
+++ Africa_SwiftUI/Africa_SwiftUI/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Africa_SwiftUI
+//
+//  Created by Junyeong Park on 2022/05/17.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("MapView")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
