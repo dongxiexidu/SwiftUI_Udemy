5e645999761f6516b30d791df1f000b962170316
PJunyeong <j_aion@naver.com>
2022-05-18 23:36:40 +0900

    feat: DetailView

---
+++ Africa_SwiftUI/Africa_SwiftUI/View/AnimalDetailView.swift
@@ -0,0 +1,55 @@
+//
+//  AnimalDetailView.swift
+//  Africa_SwiftUI
+//
+//  Created by Junyeong Park on 2022/05/18.
+//
+
+import SwiftUI
+
+struct AnimalDetailView: View {
+    let animal: Animal
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(animal.name.uppercased())
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.primary)
+                    .background(
+                        Color.accentColor
+                            .frame(height: 6)
+                            .offset(y: 24)
+                    )
+                
+                Text(animal.headline)
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(.accentColor)
+                    .padding(.horizontal)
+                
+                Group {
+                    HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
+                    
+                    InsetGalleryView(animal: animal)
+                }
+                .padding(.horizontal)
+            }
+            .navigationTitle("Learn about \(animal.name)")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct AnimalDetailView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    static var previews: some View {
+        AnimalDetailView(animal: animals[0])
+    }
+}
---
+++ Africa_SwiftUI/Africa_SwiftUI/View/HeadingView.swift
@@ -0,0 +1,33 @@
+//
+//  HeadingView.swift
+//  Africa_SwiftUI
+//
+//  Created by Junyeong Park on 2022/05/18.
+//
+
+import SwiftUI
+
+struct HeadingView: View {
+    var headingImage: String
+    var headingText: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: headingImage)
+                .foregroundColor(.accentColor)
+                .imageScale(.large)
+            Text(headingText)
+                .font(.title)
+                .fontWeight(.bold)
+        }
+        .padding(.vertical)
+    }
+}
+
+struct HeadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
