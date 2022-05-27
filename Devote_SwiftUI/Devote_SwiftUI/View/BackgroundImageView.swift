ab1c9f737603a0217b51d62ac239ca58f91aaae4
PJunyeong <j_aion@naver.com>
2022-05-27 16:54:42 +0900

    style: design with ZStack

---
+++ Devote_SwiftUI/Devote_SwiftUI/View/BackgroundImageView.swift
@@ -0,0 +1,24 @@
+//
+//  BackgroundImageView.swift
+//  Devote_SwiftUI
+//
+//  Created by Junyeong Park on 2022/05/27.
+//
+
+import SwiftUI
+
+struct BackgroundImageView: View {
+    var body: some View {
+        Image("rocket")
+            .antialiased(true)
+            .resizable()
+            .scaledToFill()
+            .ignoresSafeArea(.all)
+    }
+}
+
+struct BackgroundImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundImageView()
+    }
+}
--- Devote_SwiftUI/Devote_SwiftUI/View/ContentView.swift
+++ Devote_SwiftUI/Devote_SwiftUI/View/ContentView.swift
@@ -57,55 +57,73 @@ struct ContentView: View {
             }
         }
     }
-
+    
     var body: some View {
-        NavigationView {
-            VStack(spacing: 16) {
-                VStack {
-                    TextField("New Task", text: $task)
-                        .padding()
-                        .background(
-                            Color(UIColor.systemGray6)
-                        )
-                    .cornerRadius(10)
-                    Button(action: {
-                        addItem()
-                    }, label: {
-                        Spacer()
-                        Text("SAVE")
-                        Spacer()
-                    })
-                    .disabled(isButtonDisabled)
-                    .padding()
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .background(isButtonDisabled ? .gray : .pink)
-                    .cornerRadius(10)
-                } //: VSTACK
-                .padding()
-                List {
-                    ForEach(items) { item in
-                        VStack(alignment: .leading) {
-                            Text(item.task ?? "")
-                                .font(.headline)
-                                .fontWeight(.bold)
-                            Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                                .font(.footnote)
-                                .foregroundColor(.gray)
-                        } //: LIST ITEM
-                    }
-                    .onDelete(perform: deleteItems)
-                } //: LIST
-            } //: VSTACK
-            .navigationTitle("Daily Tasks")
-            .navigationBarTitleDisplayMode(.large)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-            }
+      NavigationView {
+        ZStack {
+          // MARK: - MAIN VIEW
+          VStack {
+            // MARK: - HEADER
+              VStack(spacing: 16) {
+                  TextField("New Task", text: $task)
+                      .padding()
+                      .background(
+                        Color(UIColor.systemGray6)
+                      )
+                      .cornerRadius(10)
+                  Button(action: {
+                      addItem()
+                  }, label: {
+                      Spacer()
+                      Text("SAVE")
+                      Spacer()
+                  })
+                  .disabled(isButtonDisabled)
+                  .padding()
+                  .font(.headline)
+                  .foregroundColor(.white)
+                  .background(isButtonDisabled ? .gray : .pink)
+                  .cornerRadius(10)
+              } //: VSTACK
+              .padding()
+            // MARK: - NEW TASK BUTTON
+            
+            // MARK: - TASKS
+            
+              List {
+                  ForEach(items) { item in
+                      VStack(alignment: .leading) {
+                          Text(item.task ?? "")
+                              .font(.headline)
+                              .fontWeight(.bold)
+                          Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                              .font(.footnote)
+                              .foregroundColor(.gray)
+                      } //: LIST ITEM
+                  }
+                  .onDelete(perform: deleteItems)
+              } //: LIST
+              .listStyle(InsetGroupedListStyle())
+              .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 12)
+              .padding(.vertical, 0)
+              .frame(maxWidth: 640)
+          
+          // MARK: - NEW TASK ITEM
+        } //: ZSTACK
+        .onAppear() {
+          UITableView.appearance().backgroundColor = UIColor.clear
         }
+        .navigationBarTitle("Daily Tasks", displayMode: .large)
+        .background(
+          BackgroundImageView()
+        )
+        .background(
+          backgroundGradient.ignoresSafeArea(.all)
+        )
+      } //: NAVIGATION
+      .navigationViewStyle(StackNavigationViewStyle())
     }
+  }
 }
 
 // MARK: - PREVIEW
