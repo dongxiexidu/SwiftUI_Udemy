ee2bfb070d7c7b84fd295c88e2388587e0304fee
PJunyeong <j_aion@naver.com>
2022-06-01 16:40:14 +0900

    style: blend effect

--- Devote_SwiftUI/Devote_SwiftUI/View/ContentView.swift
+++ Devote_SwiftUI/Devote_SwiftUI/View/ContentView.swift
@@ -106,8 +106,12 @@ struct ContentView: View {
               .padding(.vertical, 0)
               .frame(maxWidth: 640)
         } //: VSTACK
+          .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
+          .transition(.move(edge: .bottom))
+          .animation(.easeOut(duration: 0.5), value: showNewTaskItem)
             if showNewTaskItem {
-              BlankView()
+                BlankView(backgroundColor: isDarkMode ? Color.black : Color.gray,
+                          backgroundOpacity: isDarkMode ? 0.3 : 0.5)
                 .onTapGesture {
                   withAnimation() {
                     showNewTaskItem = false
@@ -124,6 +128,7 @@ struct ContentView: View {
         .navigationBarHidden(true)
         .background(
           BackgroundImageView()
+            .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
         )
         .background(
           backgroundGradient.ignoresSafeArea(.all)
