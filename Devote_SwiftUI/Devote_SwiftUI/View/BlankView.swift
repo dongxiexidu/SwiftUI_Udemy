448b2a323b094a2382c2b471a35c50ac4252daf4
PJunyeong <j_aion@naver.com>
2022-06-01 15:47:38 +0900

    feet: BlankView binding

---
+++ Devote_SwiftUI/Devote_SwiftUI/View/BlankView.swift
@@ -0,0 +1,27 @@
+//
+//  BlankView.swift
+//  Devote_SwiftUI
+//
+//  Created by Junyeong Park on 2022/06/01.
+//
+
+import SwiftUI
+
+struct BlankView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(.black)
+        .opacity(0.5)
+        .blendMode(.overlay)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct BlankView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlankView()
+    }
+}
