2c9bd5a622fb2c0a30a4ed6283c4b15dd8017602
PJunyeong <j_aion@naver.com>
2022-06-03 15:24:13 +0900

    style: appIcon and accentColor done

---
+++ Notes_SwiftUI/Notes_SwiftUI WatchKit Extension/Notes_SwiftUIApp.swift
@@ -0,0 +1,19 @@
+//
+//  Notes_SwiftUIApp.swift
+//  Notes_SwiftUI WatchKit Extension
+//
+//  Created by Junyeong Park on 2022/06/03.
+//
+
+import SwiftUI
+
+@main
+struct Notes_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ Notes_SwiftUI/README.txt
@@ -0,0 +1,3 @@
+# Notes SwiftUI: Udemy SwiftUI Lecture
+
+* 2022-06-03 Start: AppIcons and Accent Colors done
