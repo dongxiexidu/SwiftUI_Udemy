27ad7e58b081f522622a6726bdbbefb7db0c3adc
PJunyeong <j_aion@naver.com>
2022-06-03 17:18:42 +0900

    feat: Local Data save and load

--- Notes_SwiftUI/Notes_SwiftUI WatchKit Extension/View/ContentView.swift
+++ Notes_SwiftUI/Notes_SwiftUI WatchKit Extension/View/ContentView.swift
@@ -13,8 +13,45 @@ struct ContentView: View {
     @State private var text: String = ""
     
     // MARK: - FUNCTON
+    
+    func getDocumentDirectory() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
+    }
+    
     func save() {
-        dump(notes)
+        do {
+            // 1. Convert the notes array to data using JSONDecoder
+            let data = try JSONEncoder().encode(notes)
+            // 2. Create a new URL to save the file using the getDocumentDirectory func
+            let url = getDocumentDirectory().appendingPathComponent("notes")
+            // 3. Write the data to the given URL
+            try data.write(to: url)
+        } catch {
+            print("Saving data has failed.")
+        }
+    }
+    
+    func load() {
+        DispatchQueue.main.async {
+            do {
+                // 1. Get the notes URL file
+                let url = getDocumentDirectory().appendingPathComponent("notes")
+                // 2. Create a new property for the data
+                let data = try Data(contentsOf: url)
+                // 3. Decode the data
+                notes = try JSONDecoder().decode([Note].self, from: data)
+            } catch {
+                // Do nothing
+            }
+        }
+    }
+    
+    func delete(offsets: IndexSet) {
+        withAnimation {
+            notes.remove(atOffsets: offsets)
+            save()
+        }
     }
     
     // MARK: - BODY
@@ -50,10 +87,37 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             } //: HSTACK
             Spacer()
-            
+            if notes.count >= 1 {
+                List {
+                    ForEach(0..<notes.count, id: \.self) { i in
+                        HStack {
+                            Capsule()
+                                .frame(width: 4)
+                                .foregroundColor(.accentColor)
+                            Text(notes[i].text)
+                                .lineLimit(1)
+                                .padding(.leading, 5)
+                            
+                        } //: HSTACK
+                    } //: LOOP
+                    .onDelete(perform: delete)
+                }
+            } else {
+                Spacer()
+                Image(systemName: "note.text")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(.gray)
+                    .opacity(0.25)
+                    .padding(25)
+                Spacer()
+            } //: LIST
             Text("\(notes.count)")
         } //: VSTACK
         .navigationTitle("Notes")
+        .onAppear(perform: {
+            load()
+        })
     }
 }
 
