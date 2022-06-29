a101ea777fe6ac4a730eb7731f0e218e682ef7da
PJunyeong <j_aion@naver.com>
2022-06-30 08:47:19 +0900

    README

---
+++ Avocado_SwiftUI/Avocado_SwiftUI/Avocado_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avocado_SwiftUIApp.swift
+//  Avocado_SwiftUI
+//
+//  Created by Junyeong Park on 2022/06/30.
+//
+
+import SwiftUI
+
+@main
+struct Avocado_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- LearnByDoing_SwiftUI/README.txt
+++ LearnByDoing_SwiftUI/README.txt
@@ -1,4 +1,4 @@
 # LearnByDoing SwiftUI: Udemy SwiftUI Lecture
 
 * 2022-06-04 Launch: AppIcon -> Asset Done
-* 2022-06-07 Start: Chore
+* 2022-06-30: Completed
