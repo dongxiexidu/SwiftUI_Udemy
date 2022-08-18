068e69fba320cd750a72aa6515309d46d07aee6c
PJunyeong <JunyeongPark@Junyeongui-MacBookPro.local>
2022-08-18 17:56:25 +0900

    #6 GeometryReader

---
+++ SwiftUI_Youtube/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/GeometryReaderBootCamp.swift
@@ -0,0 +1,41 @@
+//
+//  GeometryReaderBootCamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Junyeong Park on 2022/08/18.
+//
+
+import SwiftUI
+
+struct GeometryReaderBootCamp: View {
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0..<20) { index in
+                    GeometryReader { geometry in
+                        RoundedRectangle(cornerRadius: 20)
+                            .rotation3DEffect(
+                                Angle(degrees: getPercentage(geo: geometry) * 40),
+                                axis: (x: 0.0, y: 1.0, z: 0.0)
+                            )
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding()
+                }
+            }
+        }
+    }
+    
+    func getPercentage(geo: GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.width / 2
+        let currentX = geo.frame(in: .global).midX
+        return 1.0 - (currentX / maxDistance)
+    }
+}
+
+struct GeometryReaderBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        GeometryReaderBootCamp()
+    }
+}
