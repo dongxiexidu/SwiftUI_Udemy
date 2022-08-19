dc6c4c3ab85916af00f937ae8c959902cfefbc5d
PJunyeong <JunyeongPark@Junyeongui-MacBookPro.local>
2022-08-19 22:04:57 +0900

    #18 Weak Self

---
+++ SwiftUI_Youtube/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/WeakSelfBootCamp.swift
@@ -0,0 +1,20 @@
+//
+//  WeakSelfBootCamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Junyeong Park on 2022/08/19.
+//
+
+import SwiftUI
+
+struct WeakSelfBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeakSelfBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        WeakSelfBootCamp()
+    }
+}
