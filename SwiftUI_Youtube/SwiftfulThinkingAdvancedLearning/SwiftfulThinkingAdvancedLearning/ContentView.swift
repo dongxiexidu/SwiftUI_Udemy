7a52a6f4159b75b0fffe1771f3a0402f21380675
PJunyeong <JunyeongPark@Junyeongui-MacBookPro.local>
2022-08-20 22:07:07 +0900

    SwiftUI Advance BootCamp Start

---
+++ SwiftUI_Youtube/SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by Junyeong Park on 2022/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Youtube/SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingAdvancedLearningApp.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by Junyeong Park on 2022/08/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
