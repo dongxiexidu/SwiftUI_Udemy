7d6b45fe41b94b785e0ffaf147ec44c5b0c74de0
PJunyeong <j_aion@naver.com>
2022-08-28 14:20:10 +0900

    #3 Asnyc and Await

--- SwiftUI_Youtube/SwiftConcurrencyBootCamp/SwiftConcurrencyBootCamp/SwiftConcurrencyBootCampApp.swift
+++ SwiftUI_Youtube/SwiftConcurrencyBootCamp/SwiftConcurrencyBootCamp/SwiftConcurrencyBootCampApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftConcurrencyBootCampApp: App {
     var body: some Scene {
         WindowGroup {
-            DownloadImageAsyncBootCamp()
+            AsyncAwaitBootCamp()
         }
     }
 }
